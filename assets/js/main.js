25e7b3a8f73b918b74975f0c1b1125b6e26e672a
John Dalmolin <82729254+john-dalmolin@users.noreply.github.com>
2024-05-02 04:04:30 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== Open =====*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== Close =====*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  // When we click on each nav__link, we remove the show-menu classname
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+  // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
+
+/*=============== SWIPER FAVORITES ===============*/
+let swiperFavorite = new Swiper(".favorite__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  grabCursor: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: false, // animations repeat
+});
+
+sr.reveal(`.home__social, .favorite__container, .sponsor__container, .footer`);
+sr.reveal(`.home__title span:nth-child(1)`, { origin: "left", opacity: 1 });
+sr.reveal(`.home__title span:nth-child(3)`, { origin: "left", opacity: 1 });
+sr.reveal(`.home__tooltip, .home__button, .model__button`, {
+  origin: "bottom",
+});
+sr.reveal(`.about__data`, { origin: "left" });
+sr.reveal(`.about__img, .model__tooltip`, { origin: "right" });
